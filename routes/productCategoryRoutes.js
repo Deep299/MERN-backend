5242af9e695e9acb117ab68229ee74268625f6c7
AkshatPatel15 <akshat15599@gmail.com>
2024-12-27 19:27:50 -0500

    adding basic auth

--- routes/productCategoryRoutes.js
+++ routes/productCategoryRoutes.js
@@ -7,13 +7,22 @@ const {
   getProductCategory,
   updateProductCategory,
 } = require("../controllers/productCategoryContoller");
+const basicAuth = require("../config/basicAuth");
 const router = express.Router();
 
-router.post("/api/productCategory/add", saveProductCategory);
-router.get("/api/productCategory/getAll", getAllProductCategories);
-router.get("/api/productCategory/get/:id", getProductCategory);
-router.delete("/api/productCategory/delete/:id", deleteProductCategory);
-router.delete("/api/productCategory/deleteMany", deleteManyProductCategories);
-router.put("/api/productCategory/update/:id", updateProductCategory);
+router.post("/api/productCategory/add", basicAuth, saveProductCategory);
+router.get("/api/productCategory/getAll", basicAuth, getAllProductCategories);
+router.get("/api/productCategory/get/:id", basicAuth, getProductCategory);
+router.delete(
+  "/api/productCategory/delete/:id",
+  basicAuth,
+  deleteProductCategory
+);
+router.delete(
+  "/api/productCategory/deleteMany",
+  basicAuth,
+  deleteManyProductCategories
+);
+router.put("/api/productCategory/update/:id", basicAuth, updateProductCategory);
 
 module.exports = router;
